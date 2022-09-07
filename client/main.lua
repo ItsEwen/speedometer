3ed6fce332015b4fafd549edb6b5fc3e47978a75
Ewen <85877345+ItsEwen@users.noreply.github.com>
2022-09-07 22:59:50 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,34 +1,33 @@
-RegisterNetEvent("speedometer:show")
-AddEventHandler("speedometer:show", function(isMetric, speed, rpm, gear) 
-    SendNUIMessage({
-        action = "show",
-        isMetric = isMetric,
-        speed = speed,
-        rpm = rpm,
-        gear = gear
-    })
-end)
+AddEventHandler("gameEventTriggered", function(eventName, eventArguments)
+    if eventName == 'CEventNetworkPlayerEnteredVehicle' then 
+        local entityId = eventArguments[2]
+        if DoesEntityExist(entityId) then 
+			while GetVehiclePedIsIn(PlayerPedId(), false) == entityId do 
+				Wait(0)
+				local playerPed = PlayerPedId()
+				local playerVehicle = GetVehiclePedIsIn(playerPed, false)
+				local vehicleSeat = GetPedInVehicleSeat(playerVehicle, -1)
 
-RegisterNetEvent("speedometer:hide")
-AddEventHandler("speedometer:hide", function() 
-    SendNUIMessage({
-        action = "hide"
-    })
-end)
+				if(playerVehicle ~= 0 ) and (not IsPauseMenuActive()) and vehicleSeat == playerPed then
+                    HideHudComponentThisFrame(7)
+                    HideHudComponentThisFrame(9)
+                    SendNUIMessage({
+                        action = "show",
+                        isMetric = ShouldUseMetricMeasurements(),
+                        speed = math.ceil(GetEntitySpeed(playerVehicle) * 3.6),
+                        rpm = GetVehicleCurrentRpm(playerVehicle),
+                        gear = GetVehicleCurrentGear(playerVehicle)
+                    })
+				else
+                    SendNUIMessage({
+                        action = "hide"
+                    })
+				end
+			end
 
-Citizen.CreateThread(function() 
-    while true do
-        local pedVehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
-        if pedVehicle ~= 0 and GetIsVehicleEngineRunning(pedVehicle) then
-            TriggerEvent("speedometer:show", ShouldUseMetricMeasurements(), GetEntitySpeed(pedVehicle), GetVehicleCurrentRpm(pedVehicle), GetVehicleCurrentGear(pedVehicle)) 
-        else
-            TriggerEvent("speedometer:hide")
-        end
-
-        HideHudComponentThisFrame(7)
-        HideHudComponentThisFrame(9)
-
-
-        Citizen.Wait(1)
-    end
+            SendNUIMessage({
+                action = "hide"
+            })
+		end
+	end
 end)
